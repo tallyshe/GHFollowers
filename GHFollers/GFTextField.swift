80201eaf4830f1f6393f764941f82a1b1bab280c
karhoshe <karhoshe@me.com>
2020-07-18 04:52:27 +0800

    Create Search Screen UI Autolayout programmatic

---
+++ GHFollers/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  GHFollers
+//
+//  Created by Kar Ho She on 18/7/2020.
+//  Copyright © 2020 Kar Ho She. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius          = 10
+        layer.borderWidth           = 2
+        layer.borderColor           = UIColor.systemGray4.cgColor
+        
+        textColor                   = .label
+        tintColor                   = .label
+        textAlignment               = .center
+        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth   = true
+        minimumFontSize             = 12
+        
+        backgroundColor             = .tertiarySystemBackground
+        autocorrectionType          = .no
+        
+        placeholder = "Enter a username"
+    }
+}
