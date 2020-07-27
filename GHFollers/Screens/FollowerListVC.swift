44f5f413dee83f45c630e3c34cda442c0d28626a
karhoshe <karhoshe@me.com>
2020-07-28 06:25:48 +0800

    Create Network Call = Get Follower Refactor Error Messages (errorMessage.swift confirm to Error Protocol)

--- GHFollers/Screens/FollowerListVC.swift
+++ GHFollers/Screens/FollowerListVC.swift
@@ -16,6 +16,17 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
+            switch result{
+                
+            case.success(let followers):
+                print(followers)
+                
+            case.failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happen", message: error.rawValue, buttonTitle: "OK")
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
