9f9e11b631e21242c8e7ed0ba19ffd40b58a0dbf
karhoshe <karhoshe@me.com>
2020-09-27 16:46:49 +0800

    Completing our search functionality Modal Presentation Ternary Operators

--- GHFollers/Screens/FollowerListVC.swift
+++ GHFollers/Screens/FollowerListVC.swift
@@ -17,6 +17,7 @@ class FollowerListVC: UIViewController {
     var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
+    var isSearching = false
         
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -118,6 +119,15 @@ extension FollowerListVC: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray = isSearching ? filteredFollowers : followers
+        let follower = activeArray[indexPath.item]
+        let destVC          = UserInfoVC()
+        destVC.username     = follower.login
+        let navController   = UINavigationController(rootViewController: destVC)
+        present(navController, animated: true)
+    }
 }
 
 
@@ -125,11 +135,13 @@ extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
     
     func updateSearchResults(for searchController: UISearchController) {
         guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        isSearching = true
         filteredFollowers = followers.filter{ $0.login.lowercased().contains(filter.lowercased())}
         updateData(on: filteredFollowers)
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
         updateData(on: followers)
     }
     
---
+++ GHFollers/Screens/UserInfoVC.swift
@@ -0,0 +1,28 @@
+//
+//  UserInfoVC.swift
+//  GHFollers
+//
+//  Created by Kar Ho She on 26/9/2020.
+//  Copyright © 2020 Kar Ho She. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username!)
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+
+}
