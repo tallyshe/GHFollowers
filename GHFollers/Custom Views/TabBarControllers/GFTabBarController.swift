d63ae8b81e4ef89ff60b3bdc72f0de241da38134
karhoshe <karhoshe@me.com>
2020-10-24 01:27:51 +0800

    UITabBarController, Screen Sizes, Constants & More
    Custom UITabBarcontroller Class
    Adjusting for specific screen sizes
    Creating an Images constants enum
    TextField optimizations
    Bug Fixes

---
+++ GHFollers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  GFTabBarController.swift
+//  GHFollers
+//
+//  Created by Kar Ho She on 11/10/2020.
+//  Copyright © 2020 Kar Ho She. All rights reserved.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor     = .systemGreen
+        viewControllers                     = [createSearchNC(), createFavoritesNC()]
+
+    }
+    
+    func createSearchNC() -> UINavigationController{
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController{
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+}
