113004bc740774956aca219f454809d328ad1dc4
karhoshe <karhoshe@me.com>
2020-11-03 01:25:02 +0800

    Persistence Manager, UITableView Extension & Bug Fixes
    Removal of unnecessary code
    UITableView Extensions
    A bug fix for our swipe to delete

---
+++ GHFollers/Extensions/UITableView+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UITableView+Ext.swift
+//  GHFollers
+//
+//  Created by Kar Ho She on 3/11/2020.
+//  Copyright © 2020 Kar Ho She. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async { self.reloadData() }
+    }
+    
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
+}
